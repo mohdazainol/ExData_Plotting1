f8d97dac095d0fac73838cf4751ed0b078321342
Mohd Amiruddin Zainol <mohd.amiruddin.zainol@gmail.com>
2017-04-03 13:20:06 +0100

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,12 @@
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+#Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels
+# and name each of the plot files as plot2.png
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
